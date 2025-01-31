b48889b46c5fcc70a36989d6458a06fec1a8d399
Prasad Malvi <prasadmalvi23@gmail.com>
2025-01-31 20:36:29 +0530

    Initial COnfiguration and Gonna Store FIle from hat ill learn about java conecpt

---
+++ src/MainPackage/ArraysLearn/BasicofArrays.java
@@ -0,0 +1,10 @@
+package MainPackage.ArraysLearn;
+
+public class BasicofArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/MainPackage/ThreadLearn/ThreadBasics.java
@@ -0,0 +1,10 @@
+package MainPackage.ThreadLearn;
+
+public class ThreadBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
