ef2d3d04eb81825c56200c5fb917a1051aa42462
Prasad Malvi <prasadmalvi23@gmail.com>
2025-02-04 20:57:17 +0530

    Array Literals

---
+++ src/Arrays/ArrayLiterals.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class ArrayLiterals {
+
+	public static void main(String[] args) {
+		//Array Literals - static array
+		int num[] = {1,2,3,4,5}; // if knows values already then only used
+		int arr[] = new int[10]; // if developer not sure of full data
+		
+	}
+
+}
